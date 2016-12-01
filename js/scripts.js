2effd27138a20bfa0330d894d3a9bc300b7a4c2a
hayleygay <hgay@eagles.ewu.edu>
2016-12-01 13:22:38 -0800

    Version 0.1
    Added default html, css, and js from Codepen weather app practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
 // My Scripts
+
+$.simpleWeather({
+    location: '99004',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('.state').text(weather.region);
+      $('.one .code').text(weather.code);
+      
+      // Change background color
+      $('body').addClass('bg-' + weather.code);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      $('body').append('<p>ERROR</p>');
+    }
+  
+  });
+
