0fcbdd903461b249250c310a9b63a437676cb176
hayleygay <hgay@eagles.ewu.edu>
2016-12-01 14:11:12 -0800

    Version 0.3
    Stylized sections, added geolocation function

--- js/scripts.js
+++ js/scripts.js
@@ -89,3 +89,15 @@ $.simpleWeather({
     }
   
   });
+
+//GEOLOCATION
+
+    // Get and store Geo Location lat/long coordinates
+        navigator.geolocation.getCurrentPosition( function(position) {
+  
+    // wait a few seconds to receive location
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        console.log( lat, long); 
+  
+    });
