4ec09a10a4a4e91b69686d38a2a322143ed628ba
hayleygay <hgay@eagles.ewu.edu>
2016-12-01 14:37:48 -0800

    Version 0.4

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,11 @@ $.simpleWeather({
       $('.spokane img').attr('src', weather.image);
       $('.spokane title').text(weather.title);
       
+      //  If this condition, show this icon
+      if ( 26 >= weather.code && 30 <= weather.code ) {
+          $('.spokane .wi').addClass('wi-day-cloudy');
+      }
+        
       // Entire weather object
       console.log(weather);
     },
@@ -92,12 +97,3 @@ $.simpleWeather({
 
 //GEOLOCATION
 
-    // Get and store Geo Location lat/long coordinates
-        navigator.geolocation.getCurrentPosition( function(position) {
-  
-    // wait a few seconds to receive location
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        console.log( lat, long); 
-  
-    });
