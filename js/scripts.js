35217e3cbca59b303f4a3585c1fab62c44203ecd
hayleygay <hgay@eagles.ewu.edu>
2016-12-01 13:45:19 -0800

    Version 0.1
    Added more cities, changed h1 css

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,91 @@
-// My Scripts
+// Spokane
 
 $.simpleWeather({
-    location: '99004',
-    woeid: '',
+    location: '99205',
     unit: 'f',
     success: function(weather) {
       
       // Display Data
-      $('.temp').text(weather.temp);
-      $('.city').text(weather.city);
-      $('.state').text(weather.region);
-      $('.one .code').text(weather.code);
-      
-      // Change background color
-      $('body').addClass('bg-' + weather.code);
+      $('.spokane .temp').text(weather.temp);
+      $('.spokane .city').text(weather.city);
+      $('.spokane img').attr('src', weather.image);
+      $('.spokane title').text(weather.title);
       
       // Entire weather object
       console.log(weather);
     },
     error: function(error) {
       // Show if weather cannot be retreived
-      $('body').append('<p>ERROR</p>');
+        $('body').append('<p>ERROR</p>');
     }
   
   });
 
+// Phoenix
+
+$.simpleWeather({
+    location: '85034',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.phoenix .temp').text(weather.temp);
+      $('.phoenix .city').text(weather.city);
+      $('.phoenix img').attr('src', weather.image);
+      $('.phoenix title').text(weather.title);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>ERROR</p>');
+    }
+  
+  });
+
+// Los Angeles
+
+$.simpleWeather({
+    location: '90045',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.la .temp').text(weather.temp);
+      $('.la .city').text(weather.city);
+      $('.la img').attr('src', weather.image);
+      $('.la title').text(weather.title);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>ERROR</p>');
+    }
+  
+  });
+
+// Seattle
+
+$.simpleWeather({
+    location: '98101',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.seattle .temp').text(weather.temp);
+      $('.seattle .city').text(weather.city);
+      $('.seattle img').attr('src', weather.image);
+      $('.seattle title').text(weather.title);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>ERROR</p>');
+    }
+  
+  });
